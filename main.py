82fa1bde9b867cc9853e6b2574bbe0ceb2451910
Patrik Nertigel <nertigel14475@gmail.com>
2024-07-19 15:18:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-our_bot_token = "123:abc"
-
-# imports
+import json
+from telegram.error import BadRequest as err_badrequest
 from telegram import Update
 from telegram.ext import (Application, ContextTypes, MessageHandler, filters)
+
+with open('data.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+
+our_bot_token = config["bot_token"]
 
 async def remove_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """This will check if the message matches group status updates and delete it"""
     message = update.message
     if (message.new_chat_members or message.left_chat_member) and message.chat.type in ('supergroup', 'group'):
         print(f"Deleted message ID {message.message_id} from {message.chat.id} at {message.date}")
-        await message.delete()
+        try:
+            await message.delete()
+        except err_badrequest as err:
+            print(err)
 
 
 def main() -> None:
     """Run the bot."""
     try:
-        print("Building Application...")
+        print("Building application...")
         application = Application.builder().token(our_bot_token).build()
 
         print("Adding remove message handler...")
